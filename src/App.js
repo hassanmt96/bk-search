67271d55586309b8b73c55196c458136b906b1e9
Hassan Tariq <66930201+hassanmt96@users.noreply.github.com>
2021-06-15 15:46:22 -0500

    testing user input for searching books

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 
 const App = () => {
+
+  const [book, setBook] = useState("");
+
+  const [result, setResult] = useState([]);
+
+  const [apiKey, setApiKey] = useState("AIzaSyBesfl57VKlcLdSi8qE3T7CBx-nwRhDNzs")
+
+
+  const handleChange = (e) => {
+
+    const book = e.target.value;
+    setBook(book)
+
+  }
+
+  const handleSubmit = (e) => {
+
+    e.preventDefault();
+
+    console.log(book);
+
+
+  }
+
   return (
     <div class="container">
       <h1>Google Books Search</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div class="form-group">
-          <input type="text" className="form-control mt-10" placeholder="I'm a bookworm."></input>
+          <input type="text" onChange={handleChange} className="form-control mt-10" placeholder="I'm a bookworm."></input>
           <button type="submit" className="btn btn danger">Search</button>
         </div>
       </form>
