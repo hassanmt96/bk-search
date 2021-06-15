e42253ebb69172158cdb059814c7a63ad10b77af
Hassan Tariq <66930201+hassanmt96@users.noreply.github.com>
2021-06-15 15:27:48 -0500

    adding the input field for the books

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import axios from 'axios'
 
 const App = () => {
   return (
-    <div>
-      Check it out
+    <div class="container">
+      <h1>Google Books Search</h1>
+      <form>
+        <div class="form-group">
+          <input type="text" className="form-control mt-10" placeholder="I'm a bookworm."></input>
+          <button type="submit" className="btn btn danger">Search</button>
+        </div>
+      </form>
     </div>
   )
 }
